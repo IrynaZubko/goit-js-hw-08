9e5189e3b889a208eddd720a6d7198af8db6fc25
Iryna Zubko <irinazubko.ntu@gmail.com>
2022-11-04 11:49:14 +0200

    change the solution

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,43 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = "feedback-form-state";
+const STORAGE_KEY = 'feedback-form-state';
 
-const formEl = document.querySelector(".feedback-form");
+const formEl = document.querySelector('.feedback-form');
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error("Set state error: ", error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Get state error: ", error.message);
+  }
+};
+
+const remove = key => {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.error("Remove state error: ", error.message);
+  }
+}
+
+export default {
+  save,
+  load,
+  remove,
+};
 
 const formData = {};
-const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+let savedData = load(STORAGE_KEY);
 
 formEl.addEventListener('input', throttle(onInputChange, 500));
 formEl.addEventListener('submit', onFormSubmit);
@@ -13,25 +45,33 @@ formEl.addEventListener('submit', onFormSubmit);
 populateInputs();
 
 function onInputChange(event) {
-    formData[event.target.name] = event.target.value;
+  if (savedData) {
+    Object.keys(savedData).map(key => {
+      formData[key] = savedData[key];
+      return;
+    });
+  }
 
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  formData[event.target.name] = event.target.value;
+
+  save(STORAGE_KEY, formData);
+  savedData = load(STORAGE_KEY);
 }
 
 function onFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    event.currentTarget.reset();
-    console.log(savedData);
-    localStorage.removeItem(STORAGE_KEY);
+  console.log(savedData);
+  event.currentTarget.reset();
+  savedData = remove(STORAGE_KEY);
 }
 
 function populateInputs() {
-    if (savedData) {
-        Object.keys(savedData).map(key => {
-            if (formEl.name = formEl[key]) {
-                formEl[key].value = savedData[key];
-            }
-        })
-    }
+  if (savedData) {
+    Object.keys(savedData).map(key => {
+      if (formEl.name = formEl[key]) {
+        formEl[key].value = savedData[key];
+      }
+    });
+  }
 }
